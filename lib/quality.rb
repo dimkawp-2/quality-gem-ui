02cf57e21fbb8a04e90ac2fb5b3e665289216035
dimkawp <dimkawp@gmail.com>
2018-11-11 11:20:08 +0200

    build new gem

---
+++ lib/quality.rb
@@ -0,0 +1,22 @@
+#lib QualityGemUi
+module Quality
+  def assert(expected:, actual:)
+    success = expected == actual
+    print success ? 'EXPECTED' : 'FAILED'
+    {success: success, expected: expected, actual: actual}
+  end
+  def run_tests
+    specs = []
+    self.class.instance_methods( false ).each do |method_name|
+      spec = public_send(method_name)
+      spec[:method_name] = method_name
+      specs << spec
+    end
+    puts
+    errored_specs = specs.reject { |spec| spec[:success] }
+    errored_specs.each { |spec|
+      puts "FAILED: #{spec[:method_name]}"
+      puts "EXPECTED: #{spec[:expected]}"
+    }
+  end
+end
